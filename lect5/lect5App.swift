4ef67aa832f34cfe6cc8bcc7926dcc19fc77cfd9
Fanny <galaxysgra@gmail.com>
2025-10-17 09:20:14 +0800

    Initial Commit

---
+++ lect5/lect5App.swift
@@ -0,0 +1,17 @@
+//
+//  lect5App.swift
+//  lect5
+//
+//  Created by Fanny on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct lect5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
